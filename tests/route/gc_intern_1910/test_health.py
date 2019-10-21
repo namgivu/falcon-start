b7a1906f2219496ce5411bfa31a0f134a502f162
namgivu <nam.gi.vu@gigacover.com>
2019-10-21 14:00:26 +0700

    unittest 0th

---
+++ tests/route/gc_intern_1910/test_health.py
@@ -0,0 +1,18 @@
+from app import api
+from falcon import testing
+
+
+def setUpModule():    pass  # nothing here for now
+def tearDownModule(): pass  # nothing here for now
+
+
+class Test(testing.TestCase):
+
+    def setUp(self):    pass  # nothing here for now
+    def tearDown(self): pass  # nothing here for now
+
+    app = api
+
+    def test(self):
+        r = self.simulate_get('/health')
+        assert r.status_code == 200
