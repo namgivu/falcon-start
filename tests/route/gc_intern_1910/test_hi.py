b537ef4fc0e2914a860cd85e5fc57d81937d7066
namgivu <namgivu@gmail.com>
2019-10-21 14:48:36 +0700

    /hi endpoint - full test

--- tests/route/gc_intern_1910/test_hi.py
+++ tests/route/gc_intern_1910/test_hi.py
@@ -9,6 +9,10 @@ def setUpModule():    pass  # nothing here for now
 def tearDownModule(): pass  # nothing here for now
 
 
+MOCKED_morning   = 8
+MOCKED_afternoon = 16
+MOCKED_evening   = 22
+
 class Test(testing.TestCase):
 
     def setUp(self):    pass  # nothing here for now
@@ -21,8 +25,22 @@ class Test(testing.TestCase):
         assert r.status_code == 200
 
 
-    @patch('util.get_now', MagicMock(return_value=datetime(2000, 11, 22, hour=8)))  # mock now() to be 8AM  #CAUTION: we cannot mock built-in datetime.datetime.now() here --> must go around via util.get_now()
+    @patch('util.get_now', MagicMock(return_value=datetime(2000, 11, 22, hour=MOCKED_morning)))  # mock now() to be 8AM  #CAUTION: we cannot mock built-in datetime.datetime.now() here --> must go around via util.get_now()
     def test_morning(self):
         r = self.simulate_get('/hi')
         assert r.status_code == 200
         assert r.text == 'Good morning'
+
+
+    @patch('util.get_now', MagicMock(return_value=datetime(2000, 11, 22, hour=MOCKED_afternoon)))  # mock now() to be 8AM  #CAUTION: we cannot mock built-in datetime.datetime.now() here --> must go around via util.get_now()
+    def test_afternoon(self):
+        r = self.simulate_get('/hi')
+        assert r.status_code == 200
+        assert r.text == 'Good afternoon'
+
+
+    @patch('util.get_now', MagicMock(return_value=datetime(2000, 11, 22, hour=MOCKED_evening)))  # mock now() to be 8AM  #CAUTION: we cannot mock built-in datetime.datetime.now() here --> must go around via util.get_now()
+    def test_afternoon(self):
+        r = self.simulate_get('/hi')
+        assert r.status_code == 200
+        assert r.text == 'Good evening'
