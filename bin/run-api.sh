3cd52321c6c7530bcc78594a0f03bde44e023938
namgivu <namgivu@gmail.com>
2019-10-24 15:11:56 +0700

    todo log file @ run-api

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -6,3 +6,7 @@ cd "$APP_HOME"
     PORT=6000; pipenv run  gunicorn  src.app:api                    -b "0.0.0.0:$PORT"  --reload
                                      #path to falcon api instance   #bind to address    #auto reload api if code changed
 cd --
+
+#TODO make this script printing log to file - extra params for :gunicorn as below
+# --timeout 666    --capture-output      --error-logfile=logfile   --access-logfile=logfile  --log-level=debug                        --threads=2                             --worker-connections=2
+# #worker timeout  #save output to file  #where to save error log  #where to save access log #TODO remove this and setting from .env  # num of threads for handling requests  # num of simultaneous clients
