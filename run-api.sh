a3f79941c2e5006371e4273407f7a8727714167d
namgivu <namgivu@gmail.com>
2019-10-24 15:05:22 +0700

    all .sh to bin/

--- run-api.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-PORT=6000; pipenv run  gunicorn  src.app:api                    -b "0.0.0.0:$PORT"  --reload
-                                 #path to falcon api instance   #bind to address    #auto reload api if code changed
--- run-test.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-PYTHONPATH=`pwd` pipenv run pytest -x
