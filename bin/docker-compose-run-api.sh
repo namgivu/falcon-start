15157857d18174686fd42af7e8dda84f57f03749
namgivu <nam.gi.vu@gigacover.com>
2019-10-31 17:45:58 +0700

    minor docker

--- bin/docker-compose-run-api.sh
+++ bin/docker-compose-run-api.sh
@@ -2,7 +2,7 @@
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
 a="$SCRIPT_HOME/.." ;                a=$(cd "$a" && pwd) ; APP_HOME="$a"
 
-if [[ -z PORT ]]; then PORT=6000; fi  # default mapped port as 6000 if not specified other
+if [[ -z $PORT ]]; then PORT=6000; fi  # default mapped port as 6000 if not specified other
 
 cd "$APP_HOME"
     # run container from the built image namgivu/falcon_start
