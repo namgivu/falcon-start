b8f1e89fc767cecdc785b388dcad7e4e0d1f55bf
namgivu <namgivu@gmail.com>
2019-11-06 21:28:04 +0700

    /bin/docker moved

--- bin/docker/start-api.sh
+++ bin/docker/start-api.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
-a="$SCRIPT_HOME/.." ;                a=$(cd "$a" && pwd) ; APP_HOME="$a"
+a="$SCRIPT_HOME/../.." ;             a=$(cd "$a" && pwd) ; APP_HOME="$a"
 
 cd "$APP_HOME"
     # stop if any running container exists
-    c=nn_falcon_start; docker stop $c; docker rm $c
+    "$SCRIPT_HOME/stop-api.sh"
 
     # load custom mapped exposed-port if nay - default 6000 if not specified
     if [[ -z $PORT ]]; then PORT=6000; fi
+
     # run it
     docker run  --name nn_falcon_start  -d                             -p $PORT:6000  namgivu/falcon_start
                 #container name         #run as daemon aka background  #port mapping  #image name
@@ -19,7 +20,7 @@ echo
 docker ps | grep -E 'falcon_start|IMAGE'
 echo "
 Container log can be viewed by (press ^C to exit watch)
-watch docker logs nn_falcon_start
+$ watch docker logs nn_falcon_start
 
-The api is ready to serve when we can see 'Listening at: http://0.0.0.0:6000'
+The api is ready to serve when we can see 'Listening at: http://0.0.0.0:$PORT'
 "
