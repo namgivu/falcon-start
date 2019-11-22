68390fab11fe5bafffba8e628e70dddf1380f9eb
namgivu <nam.gi.vu@gigacover.com>
2019-11-22 09:15:30 +0700

    review docker up

--- docker/compose-up.sh
+++ docker/compose-up.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
-a="$SCRIPT_HOME/../.." ;             a=$(cd "$a" && pwd) ; APP_HOME="$a"
+a="$SCRIPT_HOME/.." ;             a=$(cd "$a" && pwd) ; APP_HOME="$a"
 
 cd "$APP_HOME"
     # amplify if .env exists or not
     if [[ ! -f "$APP_HOME/.env" ]]; then echo "File not found .env at $APP_HOME"; exit; fi
 
     # run container from the built image namgivu/falcon_start
-    docker-compose up  -d --force-recreate  # start api at port :PORT
+    docker-compose  -f "$SCRIPT_HOME/docker-compose.yml"  up  --force-recreate  -d  # start api at port :PORT
+    #                #custom docker-compose                                     #run as background
+    #                #ref. https://stackoverflow.com/a/45158964/248616
 
     # aftermath check
     docker ps | grep -E 'falcon_start|IMAGE'
