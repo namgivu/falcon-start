93fe0b5b28c1396894febd7bf013fc88ddd5dbd3
namgivu <namgivu@gmail.com>
2020-08-01 21:19:33 +0700

    grep -E in body not in url

--- src/app.py
+++ src/app.py
@@ -5,6 +5,5 @@ from src.controller.grepapilog import GrepApiLog
 
 
 api = falcon.API()
-
 api.add_route('/health', Health() )
-api.add_route('/grepapilog/{shellgrepalike_regex}', GrepApiLog() )
+api.add_route('/grepapilog', GrepApiLog() )
--- src/controller/grepapilog.py
+++ src/controller/grepapilog.py
@@ -7,11 +7,10 @@ from src.util import LOG_FILE
 
 class GrepApiLog:
 
-    def on_get(self, req, resp, shellgrepalike_regex):
-        resp.status_code = falcon.HTTP_OK
-
+    def on_get(self, req, resp):
+        grep_regex = req.media.get('grep -E', '')  #  the shell-alike grep regex
         #region run shell's grep ref. https://stackoverflow.com/a/20004896/248616
-        cmd = ['/bin/grep', '-E', shellgrepalike_regex, LOG_FILE]
+        cmd = ['/bin/grep', '-E', grep_regex, LOG_FILE]
         spr_r = subprocess.run(cmd, stdout=subprocess.PIPE) ; r=spr_r.stdout.decode('utf-8')  # spr_r aka subprocess.run_result
         #       .          .   .    .                         binary to string
         #endregion
