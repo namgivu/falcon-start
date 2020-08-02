3615f16d36e896fd43f5ed136ff161e0164fb027
namgivu <namgivu@gmail.com>
2020-08-02 18:46:00 +0700

    regex applied when not empty

--- src/controller/grepapilog.py
+++ src/controller/grepapilog.py
@@ -8,7 +8,7 @@ from src.util import LOG_FILE
 class GrepApiLog:
 
     def on_get(self, req, resp):
-        grep_regex = req.media.get('grep -E', '')  #  the shell-alike grep regex
+        grep_regex = req.media.get('grep -E', '') if req.media else ''  #  the shell-alike grep regex
         #region run shell's grep ref. https://stackoverflow.com/a/20004896/248616
         cmd = ['/bin/grep', '-E', grep_regex, LOG_FILE]
         spr_r = subprocess.run(cmd, stdout=subprocess.PIPE) ; r=spr_r.stdout.decode('utf-8')  # spr_r aka subprocess.run_result
