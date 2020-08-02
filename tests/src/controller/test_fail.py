41d26f3c477d812eb36b395adf0b466987c2f947
namgivu <namgivu@gmail.com>
2020-08-02 13:06:04 +0700

    review test

--- tests/src/controller/test_fail.py
+++ tests/src/controller/test_fail.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import pytest
 from falcon import testing
 
@@ -13,6 +15,15 @@ def test(client):
     EXP = {
         'request'    : "<Request: GET 'http://falconframework.org/fail'>",
         'exception'  : "ERROR:division by zero - TYPE:<class 'ZeroDivisionError'>",
-        'stacktrace' : 'Traceback (most recent call last):\n  File "/home/namgivu/NN/code/_NN_/l/python/falcon-start2/.venv/lib/python3.8/site-packages/falcon/api.py", line 269, in __call__\n    responder(req, resp, **params)\n  File "/home/namgivu/NN/code/_NN_/_label_/restful/falcon-start2/src/controller/fail.py", line 8, in on_get\n    _ = 1/0  # divide by 0 on purpose to fail\nZeroDivisionError: division by zero\n'
+
+        # just listed here for reference only
+        'stacktrace': 'Traceback (most recent call last):\n  File "/home/namgivu/NN/code/_NN_/l/python/falcon-start2/.venv/lib/python3.8/site-packages/falcon/api.py", line 269, in __call__\n    responder(req, resp, **params)\n  File "/home/namgivu/NN/code/_NN_/_label_/restful/falcon-start2/src/controller/fail.py", line 8, in on_get\n    _ = 1/0  # divide by 0 on purpose to fail\nZeroDivisionError: division by zero\n'
     }
-    assert r.json  == EXP
+
+    ACT = r.json
+
+    ACT_nostacktrace = deepcopy(ACT); ACT_nostacktrace.pop('stacktrace')
+    EXP_nostacktrace = deepcopy(EXP); EXP_nostacktrace.pop('stacktrace')
+    assert ACT_nostacktrace  == EXP_nostacktrace
+
+    assert 'Traceback (most recent call last)' in ACT['stacktrace']
