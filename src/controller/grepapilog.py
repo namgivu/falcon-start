39f36112d2dd65e6981a57e22dec0c8b547dff12
namgivu <namgivu@gmail.com>
2020-08-04 22:03:31 +0700

    clean import

--- src/controller/grepapilog.py
+++ src/controller/grepapilog.py
@@ -1,5 +1,4 @@
 import falcon
-import json
 import subprocess
 
 from src.util import LOG_FILE
@@ -9,6 +8,7 @@ class GrepApiLog:
 
     def on_get(self, req, resp):
         grep_regex = req.media.get('grep -E', '') if req.media else ''  #  the shell-alike grep regex
+
         #region run shell's grep ref. https://stackoverflow.com/a/20004896/248616
         cmd = ['/bin/grep', '-E', grep_regex, LOG_FILE]
         spr_r = subprocess.run(cmd, stdout=subprocess.PIPE) ; r=spr_r.stdout.decode('utf-8')  # spr_r aka subprocess.run_result
